f6ecdd384e5428167ee7ff55c0b1a22237ed9e1e
vrrodgers <trulymissvicky1982@gmail.com>
2020-04-21 19:23:54 -0500

    added associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   belongs_to :size
   belongs_to :category
   belongs_to :category_type
+  
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,3 +1,3 @@
 class Size < ApplicationRecord
-  belongs_to :category_id
+  belongs_to :category
 end
