0715587f0da1ba8ae048af50f19a612ccaf6caf0
vrrodgers <trulymissvicky1982@gmail.com>
2020-04-27 12:06:13 -0500

    added image model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   belongs_to :size
   belongs_to :category
   belongs_to :category_type
-  
+  has_many_attached :images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   resource :products
   get 'pages/home'
   root to: 'pages#home'
